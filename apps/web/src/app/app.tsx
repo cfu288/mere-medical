1e7e9f3da646a8fb7eacb522cd3e9f8f955f8f5d
Christopher Fu <invent288cfu@gmail.com>
2022-08-03 14:50:21 -0400

    feat: add loading screen

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,21 +1,8 @@
-import '@ionic/react/css/core.css';
+import '../theme/ionic.css';
+import '../theme/tailwind.css';
 
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import '../theme/variables.css';
-
+import { ellipse, triangle } from 'ionicons/icons';
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
 import {
@@ -30,14 +17,11 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
-import { ellipse, triangle } from 'ionicons/icons';
-
-import React from 'react';
 import { PouchDbProvider } from '../components/PouchDbProvider';
-import OnPatientRedirect from '../pages/OnPatientRedirect';
-import { Routes as AppRoutes } from '../Routes';
 import ConnectionTab from '../pages/ConnectionTab';
+import OnPatientRedirect from '../pages/OnPatientRedirect';
 import TimelineTab from '../pages/TimelineTab';
+import { Routes as AppRoutes } from '../Routes';
 
 setupIonicReact();
 
