d6691d00e313ae3050f2d035f5771f0f19d3c399
Christopher Fu <invent288cfu@gmail.com>
2022-08-25 20:26:05 -0400

    build: remove unused desktop project

--- apps/desktop/src/app/api/main.preload.ts
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electron', {
-  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-  platform: process.platform,
-  webPreferences: {
-    nodeIntegration: true,
-  },
-});
