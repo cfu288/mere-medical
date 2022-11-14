e65e3694e693b1dd6e903dd17adbfc4677d6e24a
cfu288 <invent288cfu@gmail.com>
2022-11-14 17:35:58 +0000

    feat(docs): remove blog

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -40,13 +40,11 @@ const config = {
           // editUrl:
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
+        blog: false,
+          // showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
