41338ff8524f3376d9edce92dd5f95065766d626
Christopher Fu <invent288cfu@gmail.com>
2022-11-21 20:15:02 -0500

    fix(app): fix service worker

--- apps/web/src/service-worker.ts
+++ apps/web/src/service-worker.ts
@@ -51,21 +51,5 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(config.PUBLIC_URL + '/index.html')
-);
-
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
-registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: 'images',
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 256 }),
-    ],
-  })
+  createHandlerBoundToURL(config.PUBLIC_URL)
 );
