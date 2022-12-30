1c68438a9e9844412033bccc5ca715d4aaf55f31
Christopher Fu <invent288cfu@gmail.com>
2022-12-29 21:23:40 -0500

    feat(app): update empty placeholder

--- apps/web/src/models/EmptyRecordsPlaceholder.tsx
+++ apps/web/src/models/EmptyRecordsPlaceholder.tsx
@@ -1,12 +1,14 @@
+import { PlusIcon } from '@heroicons/react/20/solid';
+import { Link } from 'react-router-dom';
 import { Routes } from '../Routes';
 
 export function EmptyRecordsPlaceholder() {
   return (
-    <a
-      href={Routes.AddConnection}
-      className="focus:ring-primary-500 relative mt-4 block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2"
+    <Link
+      to={Routes.AddConnection}
+      className="focus:ring-primary-500 relative mt-4 block w-full rounded-lg  p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2"
     >
-      <svg
+      {/* <svg
         className="mx-auto h-12 w-12 text-gray-400"
         xmlns="http://www.w3.org/2000/svg"
         stroke="currentColor"
@@ -23,7 +25,39 @@ export function EmptyRecordsPlaceholder() {
       </svg>
       <span className="mt-2 block text-sm font-medium text-gray-900">
         Connect your records
-      </span>
-    </a>
+      </span> */}
+      <div className="text-center">
+        <svg
+          className="mx-auto h-12 w-12 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            vectorEffect="non-scaling-stroke"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+          />
+        </svg>
+        <h3 className="mt-2 text-sm font-medium text-gray-900">
+          No medical records
+        </h3>
+        <p className="mt-1 text-sm text-gray-500">
+          Get started by connecting to a patient portal
+        </p>
+        <div className="mt-6">
+          <button
+            type="button"
+            className="bg-primary-600 hover:bg-primary-700 inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+            Add Patient Portal
+          </button>
+        </div>
+      </div>
+    </Link>
   );
 }
