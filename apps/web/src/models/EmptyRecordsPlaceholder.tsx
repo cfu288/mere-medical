714ecb13a23cc1677ea95a2042b82c6bfb7456b3
Christopher Fu <invent288cfu@gmail.com>
2022-12-29 21:26:03 -0500

    feat(app): update empty placeholder

--- apps/web/src/models/EmptyRecordsPlaceholder.tsx
+++ apps/web/src/models/EmptyRecordsPlaceholder.tsx
@@ -4,28 +4,7 @@ import { Routes } from '../Routes';
 
 export function EmptyRecordsPlaceholder() {
   return (
-    <Link
-      to={Routes.AddConnection}
-      className="focus:ring-primary-500 relative mt-4 block w-full rounded-lg  p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2"
-    >
-      {/* <svg
-        className="mx-auto h-12 w-12 text-gray-400"
-        xmlns="http://www.w3.org/2000/svg"
-        stroke="currentColor"
-        fill="none"
-        viewBox="0 0 48 48"
-        aria-hidden="true"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-        />
-      </svg>
-      <span className="mt-2 block text-sm font-medium text-gray-900">
-        Connect your records
-      </span> */}
+    <div className="relative mt-4 block w-full rounded-lg  p-12 text-center focus:outline-none">
       <div className="text-center">
         <svg
           className="mx-auto h-12 w-12 text-gray-400"
@@ -49,15 +28,17 @@ export function EmptyRecordsPlaceholder() {
           Get started by connecting to a patient portal
         </p>
         <div className="mt-6">
-          <button
-            type="button"
-            className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-500 inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
-          >
-            <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-            Add Patient Portal
-          </button>
+          <Link to={Routes.AddConnection}>
+            <button
+              type="button"
+              className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-500 inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
+            >
+              <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+              Add Patient Portal
+            </button>
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
