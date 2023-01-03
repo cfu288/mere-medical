06b9e5ba532fb52b60791d3f0f8d0705e1b5e9e7
cfu288 <invent288cfu@gmail.com>
2023-01-03 06:21:43 +0000

    fix(web): add bottom padding to apppage

--- apps/web/src/components/AppPage.tsx
+++ apps/web/src/components/AppPage.tsx
@@ -4,7 +4,7 @@ export function AppPage(props: PropsWithChildren<{ banner: React.ReactNode }>) {
   return (
     <div className="flex h-full flex-col">
       {props.banner}
-      <div className="flex-1 flex-grow overflow-x-hidden overflow-y-scroll md:pb-0">
+      <div className="flex-1 flex-grow overflow-x-hidden overflow-y-scroll pb-24 md:pb-0">
         {props.children}
       </div>
     </div>
