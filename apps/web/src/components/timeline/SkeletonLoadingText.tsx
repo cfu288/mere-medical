258d1cb37fd558b31d894faeca43a6d44f8a995f
Christopher Fu <invent288cfu@gmail.com>
2023-02-03 11:00:31 -0500

    feat(app): tweak skeleton loader

--- apps/web/src/components/timeline/SkeletonLoadingText.tsx
+++ apps/web/src/components/timeline/SkeletonLoadingText.tsx
@@ -1,7 +1,7 @@
 export function SkeletonLoadingText() {
   return (
-    <div className="flex h-4 animate-pulse flex-row items-center">
-      <div className="mt-2 h-4 w-32 rounded-md bg-gray-100 md:h-4 "></div>
+    <div className="flex h-4 animate-pulse flex-row items-center md:mb-1">
+      <div className="h-4 w-32 rounded-md bg-gray-100 md:mt-2 md:h-4"></div>
     </div>
   );
 }
