8810c942c8a7f8fc65a63df822bb8f75dda58c2d
Christopher Fu <invent288cfu@gmail.com>
2023-02-03 23:56:26 -0500

    fix(web): apply line break on title only on small screens

--- apps/web/src/components/timeline/TimelineCardTitle.tsx
+++ apps/web/src/components/timeline/TimelineCardTitle.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export function TimelineCardTitle({ children }: PropsWithChildren) {
   return (
-    <p className="break-all pb-2 text-sm font-bold capitalize text-gray-900 md:text-base">
+    <p className="break-all pb-2 text-sm font-bold capitalize text-gray-900 md:break-normal md:text-base">
       {String(children).toLowerCase()}
     </p>
   );
