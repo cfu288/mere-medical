356540651eec62266409bd5bbc5ca7b3f7fdde03
Christopher Fu <invent288cfu@gmail.com>
2023-06-03 20:57:56 -0400

    feat(app): disable pwacompat for now

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import * as ReactDOM from 'react-dom/client';
 import App from './app/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import 'pwacompat';
+// import 'pwacompat';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
