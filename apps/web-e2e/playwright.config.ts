2be231a34ccab56e24dc1712a7deed9d56861ea6
Christopher Fu <invent288cfu@gmail.com>
2023-11-02 17:27:42 -0400

    fix: height of password screen on ios styling

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -4,7 +4,6 @@ import { baseConfig } from '../../playwright.config.base';
 
 const config: PlaywrightTestConfig = {
   ...baseConfig,
-  timeout: 5 * 60 * 1000,
 };
 
 export default config;
--- playwright.config.base.ts
+++ playwright.config.base.ts
@@ -5,8 +5,9 @@ const baseURL = process.env.E2E_BASE_URL || 'http://localhost:4200/';
 export const baseConfig: PlaywrightTestConfig = {
   retries: 2,
   maxFailures: 3,
-  timeout: 30000,
   use: {
     baseURL,
   },
+  timeout: 5 * 60 * 1000,
+  reporter: process.env.CI ? 'dot' : 'list',
 };
