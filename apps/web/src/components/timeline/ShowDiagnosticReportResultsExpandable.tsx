ebcad9ed77198e76e89dd4f7cbbe92b41310bbc7
Christopher Fu <invent288cfu@gmail.com>
2023-12-06 16:47:45 -0500

    feat: fix react warnings, remove console.log

--- apps/web/src/components/timeline/ShowDiagnosticReportResultsExpandable.tsx
+++ apps/web/src/components/timeline/ShowDiagnosticReportResultsExpandable.tsx
@@ -394,10 +394,10 @@ export function ResultRow({
                                         ?.value
                                     ).line
                                   }
-                                  stroke-width="1.5"
+                                  strokeWidth="1.5"
                                   stroke="black"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
                                   fill="transparent"
                                   vectorEffect="non-scaling-stroke"
                                 />
@@ -433,13 +433,13 @@ export function ResultRow({
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        strokeWidth="1.5"
                         stroke="currentColor"
                         className="h-6 w-6"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
                         />
                       </svg>
--- apps/web/src/pages/SummaryTab.tsx
+++ apps/web/src/pages/SummaryTab.tsx
@@ -330,8 +330,6 @@ function SummaryTab() {
   const [{ meds, cond, imm, careplan, allergy, is_pinned, initialized }] =
     useSummaryData();
 
-  console.log(is_pinned);
-
   return (
     <AppPage banner={<GenericBanner text="Summary" />}>
       {!initialized ? (
@@ -391,13 +389,13 @@ function SummaryTab() {
                                       xmlns="http://www.w3.org/2000/svg"
                                       fill="none"
                                       viewBox="0 0 24 24"
-                                      stroke-width="1.5"
+                                      strokeWidth="1.5"
                                       stroke="currentColor"
                                       className="h-2 w-2"
                                     >
                                       <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                         d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
                                       />
                                     </svg>
