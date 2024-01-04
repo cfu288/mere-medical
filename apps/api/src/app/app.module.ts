6f3a72a8d9336fd5416efa963089df3965cdc87a
Christopher Fu <invent288cfu@gmail.com>
2024-01-04 18:03:36 -0500

    fix: fix logging if no DSN is provided

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -22,24 +22,29 @@ if (checkIfSentryConfigured()) {
   imports.unshift(
     LoggerModule.forRoot({
       pinoHttp: {
-        level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
-        transport:
-          process.env.NODE_ENV === 'development'
-            ? { target: 'pino-pretty' }
-            : {
-                target: 'pino-sentry-transport',
-                options: {
-                  sentry: {
-                    dsn: process.env.SENTRY_DSN,
-                    // additional options for sentry
-                  },
-                  withLogRecord: true, // default false - send the log record to sentry as a context.(if its more then 8Kb Sentry will throw an error)
-                  tags: ['id'], // sentry tags to add to the event, uses lodash.get to get the value from the log record
-                  context: ['hostname'], // sentry context to add to the event, uses lodash.get to get the value from the log record,
-                  minLevel: 20, // which level to send to sentry
-                  skipSentryInitialization: true, // default false - if you want to initialize sentry by yourself
-                },
-              },
+        level: 'info',
+        transport: {
+          target: 'pino-sentry-transport',
+          options: {
+            sentry: {
+              dsn: process.env.SENTRY_DSN,
+              // additional options for sentry
+            },
+            withLogRecord: true, // default false - send the log record to sentry as a context.(if its more then 8Kb Sentry will throw an error)
+            tags: ['id'], // sentry tags to add to the event, uses lodash.get to get the value from the log record
+            context: ['hostname'], // sentry context to add to the event, uses lodash.get to get the value from the log record,
+            minLevel: 20, // which level to send to sentry
+          },
+        },
+      },
+    })
+  );
+} else {
+  imports.unshift(
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: 'info',
+        transport: { target: 'pino-pretty' },
       },
     })
   );
