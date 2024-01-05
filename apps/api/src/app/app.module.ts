48a04477916c9959a06b3b19ccdc41dc3e19b190
Christopher Fu <invent288cfu@gmail.com>
2024-01-05 11:10:39 -0500

    feat: update sentry error threshold to 30

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -33,7 +33,7 @@ if (checkIfSentryConfigured()) {
             withLogRecord: true, // default false - send the log record to sentry as a context.(if its more then 8Kb Sentry will throw an error)
             tags: ['id'], // sentry tags to add to the event, uses lodash.get to get the value from the log record
             context: ['hostname'], // sentry context to add to the event, uses lodash.get to get the value from the log record,
-            minLevel: 20, // which level to send to sentry
+            minLevel: 30, // which level to send to sentry
           },
         },
       },
