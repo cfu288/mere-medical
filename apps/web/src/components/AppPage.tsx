205f547de6cffd2f955f80f7b33196b08dc79c7d
Christopher Fu <invent288cfu@gmail.com>
2024-01-07 21:40:27 -0500

    feat: add padding to bottom of AppPages on small screens

--- apps/web/src/components/AppPage.tsx
+++ apps/web/src/components/AppPage.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react';
 
 export function AppPage(props: PropsWithChildren<{ banner: React.ReactNode }>) {
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex h-full flex-col pb-4 sm:pb-2">
       {props.banner}
       <div className="flex-1 flex-grow overflow-x-hidden">{props.children}</div>
     </div>
