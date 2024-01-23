9edc5eeaab06c221c6e8e99c21cae77118536d9f
Christopher Fu <invent288cfu@gmail.com>
2024-01-23 15:36:15 -0500

    refactor: move helper funcitons

--- apps/web/src/pages/MereAITab.tsx
+++ apps/web/src/pages/MereAITab.tsx
@@ -22,216 +22,28 @@ import { DatabaseCollections } from '../components/providers/DatabaseCollections
 import { UserDocument } from '../models/user-document/UserDocument.type';
 import { RxDatabase } from 'rxdb';
 
-type ChatMessage = {
-  user: 'AI' | 'USER';
-  text: string;
-  timestamp?: Date;
-  id: string;
-};
-
-function generateRandomQuestion() {
-  const questions = [
-    'How are my liver enzymes?',
-    'When was my last flu shot?',
-    'What was my blood pressure last visit?',
-    'How is my cholesterol?',
-    'What are my allergies?',
-    'What are my medications?',
-    // Add more questions as needed
-  ];
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  return questions[randomIndex];
-}
-
-const prepareDataForOpenAI = async ({
-  data,
-  db,
-  user,
-}: {
-  data: Record<string, ClinicalDocument<BundleEntry<FhirResource>>[]>;
-  db: RxDatabase<DatabaseCollections>;
-  user: UserDocument;
-}) => {
-  const dataAsList: Set<string> = new Set();
-
-  for (const itemList of Object.values(data)) {
-    for (const item of itemList) {
-      const minilist = prepareClinicalDocumentForVectorization(item).docList;
-      const texts = minilist.map((i) => i.text);
-      texts.forEach((t) => dataAsList.add(t));
-
-      if (
-        item.data_record.raw.resource?.resourceType === 'DiagnosticReport' ||
-        item.data_record.raw.resource?.resourceType === 'Observation'
-      ) {
-        const loinc = item.metadata?.loinc_coding || [];
-        const relatedDocs = await getRelatedLoincLabs({
-          loinc,
-          db,
-          user,
-          limit: 5,
-        });
-        const relatedList = relatedDocs.flatMap(
-          (i) => prepareClinicalDocumentForVectorization(i).docList,
-        );
-        relatedList.forEach((i) => dataAsList.add(i.text));
-      }
-    }
-    if (dataAsList.size > 100) {
-      break;
-    }
-  }
-
-  return [...dataAsList];
-};
-
-const callOpenAI = async ({
-  query,
-  messages,
-  openAiKey,
-  preparedData,
-  streamingCallback,
-  user,
-}: {
-  query: string;
-  messages: ChatMessage[];
-  openAiKey?: string;
-  preparedData: string[];
-  streamingCallback?: (message: string) => void;
-  user: UserDocument;
-}) => {
-  const promptMessages = [
-    {
-      role: 'system',
-      content:
-        'You are a medical AI assistant. A patient is asking you a question. Please address the patient directly and answer the question.',
-    },
-    {
-      role: 'system',
-      content: `Here is some demographic information about the patient.
-Today's Date: ${new Date().toISOString()};
-${user?.first_name && user?.last_name ? 'Name: ' + (user?.first_name + ' ' + user?.last_name) : ''}
-${user?.birthday ? 'DOB: ' + user?.birthday : ''}
-${user?.birthday ? 'Age: ' + (differenceInDays(new Date(), parseISO(user?.birthday)) / 365)?.toFixed(1) : ''}
-${user.gender ? 'Gender:' + user?.gender : ''}`,
-    },
-    ...messages.slice(-5).map((m) => ({
-      role: m.user === 'AI' ? 'assistant' : 'user',
-      content: m.text,
-    })),
-    {
-      role: 'user',
-      content: query,
-    },
-    {
-      role: 'system',
-      content: `Here are some medical records that may be relevant. Ignore any information that is not relevant to the question.
-Data: ${JSON.stringify(preparedData)}`,
-    },
-  ];
-
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${openAiKey}`,
-    },
-    body: JSON.stringify({
-      model: 'gpt-4-1106-preview',
-      messages: promptMessages,
-      stream: true,
-    }),
-  });
-
-  const reader = response.body!.getReader();
-  const decoder = new TextDecoder('utf-8');
-  let responseText = '';
-
-  try {
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) {
-        break;
-      }
-      const chunk = decoder.decode(value);
-      const lines = chunk.split('\n');
-      const parsedLines = lines
-        .map((line) => line.replace(/^data: /, '').trim())
-        .filter((line) => line !== '' && line !== '[DONE]')
-        .map((line) => JSON.parse(line));
-      for (const parsedLine of parsedLines) {
-        const { choices } = parsedLine;
-        const { delta } = choices[0];
-        const { content } = delta;
-
-        if (content) {
-          if (streamingCallback) {
-            streamingCallback(content);
-          }
-          responseText += content;
-        }
-      }
-    }
-  } catch (e) {
-    console.error(e);
-  } finally {
-    reader.releaseLock();
-  }
-
-  return responseText;
-};
-
-const performRAGwithOpenAI = async ({
-  query,
-  data,
-  streamingCallback,
-  messages = [],
-  openAiKey,
-  db,
-  user,
-}: {
-  query: string;
-  data: Record<string, ClinicalDocument<BundleEntry<FhirResource>>[]>;
-  streamingCallback?: (message: string) => void;
-  messages: ChatMessage[];
-  openAiKey?: string;
-  db: RxDatabase<DatabaseCollections>;
-  user: UserDocument;
-}) => {
-  if (data) {
-    const preparedData = await prepareDataForOpenAI({ data, db, user });
-    const responseText = await callOpenAI({
-      query,
-      messages,
-      openAiKey,
-      preparedData,
-      streamingCallback,
-      user,
-    });
-    return Promise.resolve(responseText);
-  }
-  return Promise.resolve();
-};
-
 function MereAITab() {
-  const [messages, setMessages] = useState<ChatMessage[]>([
-    {
-      user: 'AI',
-      text: "Hi there! I'm the Mere AI Assistant. Ask me any question about your medical records.",
-      timestamp: new Date(),
-      id: uuid4(),
-    },
-  ]);
-  const user = useUser();
-  const db = useRxDb();
-  const vectorStorage = useVectorStorage();
-  const { experimental__openai_api_key } = useLocalConfig();
-  const [isLoadingAiResponse, setIsLoadingAiResponse] = useState(false);
-  const notificationDispatch = useNotificationDispatch();
-  const [aiLoadingText, setAiLoadingText] = useState('');
-  const scrollRef = useRef<HTMLDivElement>(null);
+  const user = useUser(),
+    db = useRxDb(),
+    vectorStorage = useVectorStorage(),
+    { experimental__openai_api_key } = useLocalConfig(),
+    notificationDispatch = useNotificationDispatch(),
+    periodText = usePeriodAnimation();
+
+  const [isLoadingAiResponse, setIsLoadingAiResponse] = useState(false),
+    [aiLoadingText, setAiLoadingText] = useState(''),
+    [messages, setMessages] = useState<ChatMessage[]>([
+      {
+        user: 'AI',
+        text: "Hi there! I'm the Mere AI Assistant. Ask me any question about your medical records.",
+        timestamp: new Date(),
+        id: uuid4(),
+      },
+    ]),
+    scrollRef = useRef<HTMLDivElement>(null);
+
   const isAiMessage = (message: ChatMessage) => message.user === 'AI';
-  const periodText = usePeriodAnimation();
+
   const callAskAI = useCallback(
     (messageText: string) => {
       if (!isLoadingAiResponse && vectorStorage) {
@@ -434,20 +246,6 @@ const MessageBubble = memo(function MessageBubble({
   );
 });
 
-function formatTimestampText(isoDate: string) {
-  return Math.abs(differenceInDays(parseISO(isoDate), new Date())) >= 1
-    ? `${formatTimestampToDay(isoDate)}`
-    : `${formatTimestampToTime(isoDate)}`;
-}
-
-function formatTimestampToDay(isoDate: string) {
-  return format(parseISO(isoDate), 'MMM dd');
-}
-
-function formatTimestampToTime(isoDate: string) {
-  return format(parseISO(isoDate), 'p');
-}
-
 const ChatInput = memo(function ChatInput({
   onSubmit,
   isLoadingAiResponse,
@@ -455,12 +253,12 @@ const ChatInput = memo(function ChatInput({
   onSubmit: (message: string) => void;
   isLoadingAiResponse: boolean;
 }) {
-  const [message, setMessage] = useState('');
-  const periodText = usePeriodAnimation();
-  const placeholderText = useMemo(
-    () => 'Ask a question (e.g., ' + generateRandomQuestion() + ')',
-    [],
-  );
+  const [message, setMessage] = useState(''),
+    periodText = usePeriodAnimation(),
+    placeholderText = useMemo(
+      () => 'Ask a question (e.g., ' + generateRandomQuestion() + ')',
+      [],
+    );
 
   return (
     <form
@@ -485,7 +283,6 @@ const ChatInput = memo(function ChatInput({
           disabled={isLoadingAiResponse}
           className={`min-w-18 whitespace-nowrap transition-all text-xs hover:scale-105 active:scale-100 shadow-indigo-500/50 hover:shadow-indigo-400/50 shadow-md hover:shadow-lg active:shadow-sm active:shadow-indigo-600/50 bg-indigo-700 text-indigo-50 hover:bg-indigo-600 rounded-md disabled:opacity-50 disabled:cursor-not-allowed p-2 py-3 disabled:bg-gradient-to-br disabled:scale-100 disabled:shadow-indigo-200/50 disabled:from-indigo-700 disabled:via-purple-700 disabled:to-indigo-700 font-bold hover:bg-gradient-to-br hover:from-indigo-700 hover:via-purple-700 hover:to-indigo-700 background-animate`}
         >
-          {' '}
           {isLoadingAiResponse ? <>{periodText}</> : <>{`✨ Ask AI`} </>}
           <span className="sr-only">Send message</span>
         </button>
@@ -495,3 +292,207 @@ const ChatInput = memo(function ChatInput({
 });
 
 export default MereAITab;
+
+function formatTimestampText(isoDate: string) {
+  return Math.abs(differenceInDays(parseISO(isoDate), new Date())) >= 1
+    ? `${formatTimestampToDay(isoDate)}`
+    : `${formatTimestampToTime(isoDate)}`;
+}
+
+function formatTimestampToDay(isoDate: string) {
+  return format(parseISO(isoDate), 'MMM dd');
+}
+
+function formatTimestampToTime(isoDate: string) {
+  return format(parseISO(isoDate), 'p');
+}
+
+type ChatMessage = {
+  user: 'AI' | 'USER';
+  text: string;
+  timestamp?: Date;
+  id: string;
+};
+
+function generateRandomQuestion() {
+  const questions = [
+    'How are my liver enzymes?',
+    'When was my last flu shot?',
+    'What was my blood pressure last visit?',
+    'How is my cholesterol?',
+    'What are my allergies?',
+    'What are my medications?',
+  ];
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  return questions[randomIndex];
+}
+
+const prepareDataForOpenAI = async ({
+  data,
+  db,
+  user,
+}: {
+  data: Record<string, ClinicalDocument<BundleEntry<FhirResource>>[]>;
+  db: RxDatabase<DatabaseCollections>;
+  user: UserDocument;
+}) => {
+  const dataAsList: Set<string> = new Set();
+
+  for (const itemList of Object.values(data)) {
+    for (const item of itemList) {
+      const minilist = prepareClinicalDocumentForVectorization(item).docList;
+      const texts = minilist.map((i) => i.text);
+      texts.forEach((t) => dataAsList.add(t));
+
+      if (
+        item.data_record.raw.resource?.resourceType === 'DiagnosticReport' ||
+        item.data_record.raw.resource?.resourceType === 'Observation'
+      ) {
+        const loinc = item.metadata?.loinc_coding || [];
+        const relatedDocs = await getRelatedLoincLabs({
+          loinc,
+          db,
+          user,
+          limit: 5,
+        });
+        const relatedList = relatedDocs.flatMap(
+          (i) => prepareClinicalDocumentForVectorization(i).docList,
+        );
+        relatedList.forEach((i) => dataAsList.add(i.text));
+      }
+    }
+    if (dataAsList.size > 100) {
+      break;
+    }
+  }
+
+  return [...dataAsList];
+};
+
+const callOpenAI = async ({
+  query,
+  messages,
+  openAiKey,
+  preparedData,
+  streamingCallback,
+  user,
+}: {
+  query: string;
+  messages: ChatMessage[];
+  openAiKey?: string;
+  preparedData: string[];
+  streamingCallback?: (message: string) => void;
+  user: UserDocument;
+}) => {
+  const promptMessages = [
+    {
+      role: 'system',
+      content:
+        'You are a medical AI assistant. A patient is asking you a question. Please address the patient directly and explain your answer step by step. Show your work if possible.',
+    },
+    {
+      role: 'system',
+      content: `Here is some demographic information about the patient.
+Today's Date: ${new Date().toISOString()};
+${user?.first_name && user?.last_name ? 'Name: ' + (user?.first_name + ' ' + user?.last_name) : ''}
+${user?.birthday ? 'DOB: ' + user?.birthday : ''}
+${user?.birthday ? 'Age: ' + (differenceInDays(new Date(), parseISO(user?.birthday)) / 365)?.toFixed(1) : ''}
+${user.gender ? 'Gender:' + user?.gender : ''}`,
+    },
+    ...messages.slice(-5).map((m) => ({
+      role: m.user === 'AI' ? 'assistant' : 'user',
+      content: m.text,
+    })),
+    {
+      role: 'user',
+      content: query,
+    },
+    {
+      role: 'system',
+      content: `Here are some medical records that may be relevant. Ignore and leave out any records not relevant to the question in your response.
+Data: ${JSON.stringify(preparedData)}`,
+    },
+  ];
+
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${openAiKey}`,
+    },
+    body: JSON.stringify({
+      model: 'gpt-4-1106-preview',
+      messages: promptMessages,
+      stream: true,
+    }),
+  });
+
+  const reader = response.body!.getReader();
+  const decoder = new TextDecoder('utf-8');
+  let responseText = '';
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      const chunk = decoder.decode(value);
+      const lines = chunk.split('\n');
+      const parsedLines = lines
+        .map((line) => line.replace(/^data: /, '').trim())
+        .filter((line) => line !== '' && line !== '[DONE]')
+        .map((line) => JSON.parse(line));
+      for (const parsedLine of parsedLines) {
+        const { choices } = parsedLine;
+        const { delta } = choices[0];
+        const { content } = delta;
+
+        if (content) {
+          if (streamingCallback) {
+            streamingCallback(content);
+          }
+          responseText += content;
+        }
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    reader.releaseLock();
+  }
+
+  return responseText;
+};
+
+const performRAGwithOpenAI = async ({
+  query,
+  data,
+  streamingCallback,
+  messages = [],
+  openAiKey,
+  db,
+  user,
+}: {
+  query: string;
+  data: Record<string, ClinicalDocument<BundleEntry<FhirResource>>[]>;
+  streamingCallback?: (message: string) => void;
+  messages: ChatMessage[];
+  openAiKey?: string;
+  db: RxDatabase<DatabaseCollections>;
+  user: UserDocument;
+}) => {
+  if (data) {
+    const preparedData = await prepareDataForOpenAI({ data, db, user });
+    const responseText = await callOpenAI({
+      query,
+      messages,
+      openAiKey,
+      preparedData,
+      streamingCallback,
+      user,
+    });
+    return Promise.resolve(responseText);
+  }
+  return Promise.resolve();
+};
