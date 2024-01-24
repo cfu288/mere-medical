268469c2758f6021b90b6e3bbce8b106f12e7ddf
Christopher Fu <invent288cfu@gmail.com>
2024-01-24 18:28:00 -0500

    merge: merge stashed content

--- apps/web/src/components/settings/UserDataSettingsGroup.tsx
+++ apps/web/src/components/settings/UserDataSettingsGroup.tsx
@@ -14,6 +14,7 @@ import {
   requestPersistentStorage,
 } from '../../utils/storagePermissionUtils';
 import { Link } from 'react-router-dom';
+import React from 'react';
 
 export type ImportFields = {
   backup?: FileList;
