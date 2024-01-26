7a80c8a59c50d8b64946c981399a59eac55e880b
Christopher Fu <invent288cfu@gmail.com>
2024-01-25 22:01:05 -0500

    feat: update styling on text input in chat

--- apps/web/src/pages/MereAITab.tsx
+++ apps/web/src/pages/MereAITab.tsx
@@ -272,19 +272,19 @@ const ChatInput = memo(function ChatInput({
         onSubmit(message);
       }}
     >
-      <div className="flex flex-row items-center px-3 py-2 rounded-lg bg-gray-50 justify-center align-middle">
-        <textarea
+      <div className="flex flex-row items-center p-2 rounded-lg bg-gray-50 justify-center align-middle">
+        <input
           id="chat"
-          rows={1}
+          // rows={1}
           value={message}
-          className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500"
+          className="block mr-2 h-full p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500"
           placeholder={placeholderText}
           onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
+        ></input>
         <button
           type="submit"
           disabled={isLoadingAiResponse}
-          className={`min-w-18 whitespace-nowrap transition-all text-xs hover:scale-105 active:scale-100 shadow-indigo-500/50 hover:shadow-indigo-400/50 shadow-md hover:shadow-lg active:shadow-sm active:shadow-indigo-600/50 bg-indigo-700 text-indigo-50 hover:bg-indigo-600 rounded-md disabled:opacity-50 disabled:cursor-not-allowed p-2 py-3 disabled:bg-gradient-to-br disabled:scale-100 disabled:shadow-indigo-200/50 disabled:from-indigo-700 disabled:via-purple-700 disabled:to-indigo-700 font-bold hover:bg-gradient-to-br hover:from-indigo-700 hover:via-purple-700 hover:to-indigo-700 background-animate`}
+          className={`min-w-18 h-full whitespace-nowrap transition-all text-xs hover:scale-[102%] active:scale-100 shadow-indigo-500/50 hover:shadow-indigo-400/50 shadow-md hover:shadow-lg active:shadow-sm active:shadow-indigo-600/50 bg-indigo-700 text-indigo-50 hover:bg-indigo-600 rounded-md disabled:opacity-50 disabled:cursor-not-allowed p-2 py-3 disabled:bg-gradient-to-br disabled:scale-100 disabled:shadow-indigo-200/50 disabled:from-indigo-700 disabled:via-purple-700 disabled:to-indigo-700 font-bold hover:bg-gradient-to-br hover:from-indigo-700 hover:via-purple-700 hover:to-indigo-700 background-animate`}
         >
           {isLoadingAiResponse ? <>{periodText}</> : <>{`✨ Ask AI`} </>}
           <span className="sr-only">Send message</span>
