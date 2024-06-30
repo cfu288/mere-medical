d33f5c5287fb4418b15333d1671c616af57c52d2
Christopher Fu <invent288cfu@gmail.com>
2024-06-30 15:37:13 -0400

    build: update ga workflows

--- .github/actions/trigger-env-deploy.js
+++ .github/actions/trigger-env-deploy.js
@@ -1,18 +1,17 @@
-const { Octokit } = require('@octokit/action');
-
-const octokit = new Octokit();
-
 (async () => {
+  const { Octokit } = await import('@octokit/action');
+  const octokit = new Octokit();
+
   try {
     await octokit.request(
       `POST /repos/cfu288/ansible_inventories/actions/workflows/${process.env.WORKFLOW_ID}/dispatches`,
       {
         ref: 'main',
-      }
+      },
     );
-    console.log('Stage deploy completed');
+    console.log('Deploy completed');
   } catch (error) {
-    console.log('Error when attempting to trigger stage deploy:', error);
+    console.log('Error when attempting to trigger deploy:', error);
     process.exit(1);
   }
 })();
