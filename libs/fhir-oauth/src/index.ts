3b14f3d21d56b1751e1639e57e846e2ec64440ac
Christopher Fu <invent288cfu@gmail.com>
2026-01-22 14:39:06 -0500

    docs: remove doc string

--- libs/fhir-oauth/src/index.ts
+++ libs/fhir-oauth/src/index.ts
@@ -1,21 +1,3 @@
-/**
- * @mere/fhir-oauth - OAuth clients for FHIR servers
- *
- * @example
- * import { createEpicClient } from '@mere/fhir-oauth';
- *
- * // On login page - initiate auth and redirect
- * const client = createEpicClient({ signJwt });
- * const { url, session } = await client.initiateAuth(config);
- * sessionStorage.setItem('oauth_session', JSON.stringify(session));
- * window.location.href = url;
- *
- * // On callback page - restore session and exchange code for tokens
- * const client = createEpicClient({ signJwt });
- * const session = JSON.parse(sessionStorage.getItem('oauth_session'));
- * const tokens = await client.handleCallback(searchParams, config, session);
- */
-
 // Vendor clients - primary API
 export {
   createEpicClient,
