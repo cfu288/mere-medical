735a3b7793284b59cf7146678cf07450cf8da9b5
Stephen Rimbakusumo <Taurvi@users.noreply.github.com>
2026-04-30 18:45:01 -0700

    feat: adds support for R4 brands endpoint (#326)
    * feat: adds support for R4 brands endpoint
    
    * [chore] Ran update script one more time

--- libs/epic/fetchMetadata.ts
+++ libs/epic/fetchMetadata.ts
@@ -15,6 +15,12 @@ class TerminalColor {
     `${TerminalColor.Red}${str}${TerminalColor.Reset}`;
 }
 
+const FHIR_VERSION = {
+  DSTU2: 'DSTU2',
+  R4: 'R4',
+} as const;
+type FhirVersion = (typeof FHIR_VERSION)[keyof typeof FHIR_VERSION];
+
 type EndpointItem = {
   id: string;
   name: string;
@@ -128,13 +134,15 @@ const runBatched = async (
 (async () => {
   try {
     const args = process.argv.slice(2);
-    const fhirVersion = args.includes('--r4') ? 'R4' : 'DSTU2';
+    const fhirVersion = args.includes('--r4')
+      ? FHIR_VERSION.R4
+      : FHIR_VERSION.DSTU2;
 
     console.log(`Starting ${fhirVersion} Endpoint Metadata Fetcher for EPIC`);
 
     const endpointsUrl =
-      fhirVersion === 'R4'
-        ? process.env.R4_ENDPOINTS_URL
+      fhirVersion === FHIR_VERSION.R4
+        ? process.env.R4_BRANDS_ENDPOINT_URL
         : process.env.DSTU2_ENDPOINTS_URL;
 
     if (!endpointsUrl) {
@@ -144,7 +152,67 @@ const runBatched = async (
     }
     console.log(`Using URL: ${endpointsUrl}`);
 
-    const data = await fetch(endpointsUrl, {
+    if (fhirVersion === FHIR_VERSION.DSTU2) {
+      getAndProcessDSTU2({ url: endpointsUrl, fhirVersion: fhirVersion });
+    } else {
+      console.log('Download will start shortly');
+      getAndProcessR4({ url: endpointsUrl, fhirVersion: fhirVersion });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  /*
+   * Get Data Functions/Types
+   */
+  interface GetDataProps {
+    url: string;
+    fhirVersion: FhirVersion;
+  }
+
+  async function getAndProcessR4(props: GetDataProps): Promise<void> {
+    const response = await fetch(props.url, {
+      headers: {
+        Accept: 'application/json+fhir',
+      },
+    });
+
+    const rawResponse = await downloadAndDisplayStatus({
+      fetchResponse: response,
+    });
+    const jsonResponse = convertResponseToJson(rawResponse);
+
+    const urls: EndpointItem[] = [];
+
+    (jsonResponse as any)?.entry.forEach((i: any) => {
+      // skips resources which are not of type `Endpoint`
+      if (i.resource.resourceType !== 'Endpoint') {
+        return;
+      }
+
+      const dataUrl = i.resource?.address;
+      urls.push({
+        id: i.resource?.id,
+        name: i.resource?.name,
+        // most new data does not have a tailing slash
+        url:
+          dataUrl.endsWith('/') === true
+            ? i.resource.address
+            : `${i.resource.address}/`,
+        // managing organization name moved to `display`
+        managingOrganization: i.resource?.managingOrganization?.display,
+      });
+    });
+
+    if (!urls || !urls.length) {
+      throw new Error('No content found');
+    }
+
+    processData({ urls: urls, fhirVersion: props.fhirVersion });
+  }
+
+  async function getAndProcessDSTU2(props: GetDataProps): Promise<void> {
+    const data = await fetch(props.url, {
       headers: { Accept: 'application/json+fhir' },
     }).then((res) => res.json());
 
@@ -159,7 +227,19 @@ const runBatched = async (
       throw new Error('No content found');
     }
 
-    const initial = await runBatched(urls);
+    processData({ urls: urls, fhirVersion: props.fhirVersion });
+  }
+
+  /*
+   * Process Data Functions/Types
+   */
+  interface ProcessDataProps {
+    urls: EndpointItem[];
+    fhirVersion: FhirVersion;
+  }
+
+  async function processData(props: ProcessDataProps) {
+    const initial = await runBatched(props.urls);
     const results: EndpointMeta[] = [...initial.results];
     let failures: Failure[] = initial.failures;
     let totalSkipped = initial.skipped;
@@ -188,7 +268,7 @@ const runBatched = async (
       );
     }
 
-    if (fhirVersion === 'R4') {
+    if (props.fhirVersion === FHIR_VERSION.R4) {
       results.push({
         id: 'sandbox_epic_r4',
         name: 'Epic MyChart Sandbox (R4)',
@@ -209,7 +289,7 @@ const runBatched = async (
     }
 
     const outputPath =
-      fhirVersion === 'R4'
+      props.fhirVersion === FHIR_VERSION.R4
         ? './src/lib/data/R4Endpoints.json'
         : './src/lib/data/DSTU2Endpoints.json';
     const usable = results.filter((r) => r.token && r.authorize);
@@ -224,7 +304,9 @@ const runBatched = async (
     fs.writeFileSync(outputPath, JSON.stringify(usable, null, 2));
 
     const errorLogPath =
-      fhirVersion === 'R4' ? './errorlog-r4.json' : './errorlog-dstu2.json';
+      props.fhirVersion === FHIR_VERSION.R4
+        ? './errorlog-r4.json'
+        : './errorlog-dstu2.json';
     if (failures.length) {
       console.log(
         TerminalColor.red(
@@ -250,7 +332,103 @@ const runBatched = async (
     }
 
     console.log(TerminalColor.green('Done'));
-  } catch (e) {
-    console.error(e);
+  }
+
+  /*
+   * Utility Functions/Types
+   */
+  interface DownloadAndDisplayStatusProps {
+    fetchResponse: Response;
+  }
+
+  interface DownloadAndDisplayStatusResponse {
+    receivedLength: number;
+    chunks: Uint8Array<ArrayBuffer>[];
+  }
+
+  async function downloadAndDisplayStatus(
+    props: DownloadAndDisplayStatusProps,
+  ): Promise<DownloadAndDisplayStatusResponse> {
+    const reader = props.fetchResponse.body?.getReader();
+
+    if (reader == null) {
+      throw new Error('response body was null');
+    }
+
+    const contentLengthHeader =
+      props.fetchResponse.headers.get('Content-Length');
+
+    if (contentLengthHeader == null) {
+      throw new Error('contentLength header was null');
+    }
+
+    const contentLength = parseInt(contentLengthHeader);
+
+    let isDone = false;
+    let receivedLength = 0;
+    const chunks: Array<Uint8Array<ArrayBuffer>> = [];
+
+    while (isDone === false) {
+      const { done, value } = await reader.read();
+      isDone = done;
+
+      if (isDone === true) {
+        console.log('\n');
+        break;
+      }
+
+      if (value == null) {
+        throw new Error('reader value was null or undefined');
+      }
+
+      chunks.push(value);
+      receivedLength += value.length;
+
+      renderLoadingBar({
+        receivedLength: receivedLength,
+        contentLength: contentLength,
+        barWidth: 20,
+      });
+    }
+
+    return {
+      receivedLength: receivedLength,
+      chunks: chunks,
+    };
+  }
+
+  interface RenderLoadingBarProps {
+    receivedLength: number;
+    contentLength: number;
+    barWidth: number;
+  }
+
+  function renderLoadingBar(props: RenderLoadingBarProps): void {
+    const percentDone = Math.round(
+      (props.receivedLength * 100) / props.contentLength,
+    );
+    const visualBlockCount = Math.floor((percentDone * props.barWidth) / 100);
+    const emptyBlockCount = props.barWidth - visualBlockCount;
+    const visualLoadingBarLoaded = '█'.repeat(visualBlockCount);
+    const visualLoadingBarRemaining = '░'.repeat(emptyBlockCount);
+
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    process.stdout.write(
+      `Downloading [${TerminalColor.green(visualLoadingBarLoaded)}${visualLoadingBarRemaining}] ${percentDone}%`,
+    );
+  }
+
+  type ConvertResponseToJsonProps = DownloadAndDisplayStatusResponse;
+
+  function convertResponseToJson(props: ConvertResponseToJsonProps): JSON {
+    const allChunks = new Uint8Array(props.receivedLength);
+    let position = 0;
+    for (const chunk of props.chunks) {
+      allChunks.set(chunk, position);
+      position += chunk.length;
+    }
+    const decodedResult = new TextDecoder('utf-8').decode(allChunks);
+    return JSON.parse(decodedResult);
   }
 })().catch((e) => console.error(e));
